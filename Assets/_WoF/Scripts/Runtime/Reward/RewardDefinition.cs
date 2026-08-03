4bdad860accc0972eb39931cdcbad0a3a42ba293
projectsimtime <projectsimtime@gmail.com>
2026-08-04 01:00:59 +0300

    feat: add reward, wheel and zone scriptable objects

---
+++ Assets/_WoF/Scripts/Runtime/Reward/RewardDefinition.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace WoF.Reward
+{
+	public enum EItemRarity
+	{
+		Casual,
+		Rare,
+		Epic,
+		Legendary
+	}
+
+	public enum EItemKind
+	{
+		Cash,
+		Case,
+		Weapon,
+		Points,
+		Armor,
+		Additional
+	}
+	
+	[CreateAssetMenu(fileName = "reward_", menuName = "WoF/Reward/New Reward")]
+	public class RewardDefinition : ScriptableObject
+	{
+		[SerializeField]
+		private Sprite _sprite;
+		[SerializeField]
+		private EItemRarity _rarity;
+		[SerializeField]
+		private EItemKind _kind;
+		
+		public Sprite Sprite => _sprite;
+		public EItemRarity Rarity => _rarity;
+		public EItemKind Kind => _kind;
+	}
+}
---
+++ Assets/_WoF/Scripts/Runtime/Zone/ZoneRules.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using WoF.Reward;
+
+namespace WoF.Zone
+{
+	[CreateAssetMenu(fileName = "zoneRule_", menuName = "WoF/Zone/New Zone Rule", order = 0)]
+	public class ZoneRules : ScriptableObject
+	{
+		[Serializable]
+		public struct ZoneOverride
+		{
+			[SerializeField] 
+			private uint _zoneNumber;
+			[SerializeField]
+			private RewardDefinition[] _rewards;
+
+			public uint ZoneNumber => _zoneNumber;
+			public RewardDefinition[] Rewards => _rewards;
+		}
+		
+		[SerializeField]
+		private ZoneOverride[] _zoneOverrides;
+
+		public ZoneOverride[] ZoneOverrides => _zoneOverrides;
+	}
+}
